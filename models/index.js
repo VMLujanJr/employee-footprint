a17cc13f25f4f71df3a488e01c493f412b7acb54
VMLujanJr <vmlujanjr@outlook.com>
2022-05-22 10:41:24 -0500

    finished employee, role, and department models

--- models/index.js
+++ models/index.js
@@ -1,7 +1,11 @@
 const Department = require('./Department.js');
+const Role = require('./Role.js');
+const Employee = require('./Employee.js');
 
 // create associations
 
 module.exports = {
-    Department
+    Department,
+    Role,
+    Employee
 }
